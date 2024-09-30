dc6cd4610bda4c37f95ed017f0355ceb5f8ceee1
Vinicius <148554614+JamesCJ2@users.noreply.github.com>
2024-09-30 09:39:47 -0300

    Update loginADM.js

--- assets/js/loginADM.js
+++ assets/js/loginADM.js
@@ -1,43 +1,25 @@
-let ADM = [];
-let adms = {};
-let JSONadm = localStorage.getItem('ADM');
-ADM = JSON.parse(JSONadm);
-if(ADM == null){
-    ADM = [];
- };
-
-ADM = [
-    adms = {
-              email: "viniadm@gmail.com",
-              senha: "admin"
-            }
-];
 
 let emailADM = document.getElementById('email').value;
 let senhaADM = document.getElementById('senha').value;
 
 function logarADM(){
    
-for (adms in ADM){
-    //funcao para verificar login e redirecionar à pagina
-    if(emailADM && senhaADM){ 
-        let LoginExistente = ADM.find(adms => adms.email === emailADM && adms.senha === senhaADM);
 
-        }if(LoginExistente){
-            let ADMlogado = LoginExistente;
-            localStorage.setItem('ADMlogado', JSON.stringify(ADMlogado));
+    //funcao para verificar login e redirecionar à pagina
+    if(emailADM === "admin@gmail.com" && senhaADM === "admin"){ 
+       
 
 
             //metodo usado para redirecionar caso login esteja correto
-        alert("Logado como ADM com Sucesso")
-        window.location.href = "html/AreaLogadaADM.html"
+        alert("Logado como ADM com Sucesso");
+        window.location.href = "html/AreaLogadaADM.html";
 
         }else{
         alert("Login ou Senha Invalidos!!");
         
         document.getElementById('email').value = '';
         document.getElementById('senha').value = '';
-        }
     }
+    
     };
 
