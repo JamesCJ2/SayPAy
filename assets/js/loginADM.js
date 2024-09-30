f6b24b26058907318a4e8385e87c3a47609f20d6
Vinicius <148554614+JamesCJ2@users.noreply.github.com>
2024-09-30 09:43:44 -0300

    Update loginADM.js

--- assets/js/loginADM.js
+++ assets/js/loginADM.js
@@ -8,11 +8,9 @@ function logarADM(){
     //funcao para verificar login e redirecionar à pagina
     if(emailADM === "admin@gmail.com" && senhaADM === "admin"){ 
        
-
-
             //metodo usado para redirecionar caso login esteja correto
         alert("Logado como ADM com Sucesso");
-        window.location.href = "html/AreaLogadaADM.html";
+        window.location.href = "../html/AreaLogadaADM.html";
 
         }else{
         alert("Login ou Senha Invalidos!!");
